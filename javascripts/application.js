3c1762f58195b727baa0a3639e711db0f22829ab
Mat Fukano <fukano.mat@gmail.com>
2023-02-05 17:42:16 -0800

    Added onload event listener for EmailJS, ensure clicking on input buttons doesn't collapse page

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,8 +1,26 @@
+window.onload = function() {
+  console.log(`in window.onload function from app.js`)
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault()
+    this.contact_number.value = Math.random() * 100000 | 0;
+    emailjs.sendForm('contact_service', 'contact_form', this)
+      .then(function() {
+        console.log('SUCCESS!')
+      }, function(error) {
+        console.log(`FAILED WITH ERROR: ${error}`, error)
+      })
+  })
+}
+
 // Open page quadrant
 let box = document.getElementsByClassName('box')
 Array.from(box).forEach(e => e.addEventListener('click', activateBox, false))
 
 function activateBox(e) {
+  console.log(e.target.tagName)
+  if (['INPUT', 'TEXTAREA', 'A', 'BUTTON'].some(name => name === e.target.tagName) || e.target.class === 'email-form') {
+    return
+  }
   this.classList.toggle('active')
   this.firstElementChild.classList.toggle('active')
   if (!document.querySelector('.active')) {
